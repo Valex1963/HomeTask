b5a20c0c8d79ce5d02ce3c495fdf3e79b1c86034
Valeri1963 <cvvei77@gmail.com>
2022-12-20 13:25:23 +0300

    add HomeTask1

---
+++ HomeTask1/Program.cs
@@ -0,0 +1,18 @@
+﻿Console.WriteLine("Ведите первое число:  ");
+Console.WriteLine( "Введите второе число:  ");
+
+int numberA = int.Parse(Console.ReadLine());
+int numberB = int.Parse(Console.ReadLine());
+
+if (numberA > numberB)
+{
+    Console.WriteLine("Первое число = max");
+}
+
+if (numberB > numberA)
+{
+    Console.WriteLine("Второе число = max");
+}
+
+
+
