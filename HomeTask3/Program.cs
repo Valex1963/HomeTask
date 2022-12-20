ae9ddbcf48519bf7a95b0098221af1c664d84687
Valeri1963 <cvvei77@gmail.com>
2022-12-20 13:55:38 +0300

    add HomeTask3

---
+++ HomeTask3/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.WriteLine("Введите число: ");
+
+int A = int.Parse(Console.ReadLine());
+
+if (A %2 ==0)
+{
+    Console.Write("Введенное число является четным");
+    Console.Read();
+
+}
+if (A %2 == 1)
+{
+    Console.Write("Введенное число нечетное");
+    Console.Read();
+}
