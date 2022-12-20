ec19669581705fb356b39f279a7f8c22c3286358
Valeri1963 <cvvei77@gmail.com>
2022-12-20 14:10:11 +0300

    add HomeTask4

---
+++ HomeTask4/Program.cs
@@ -0,0 +1,16 @@
+﻿System.Console.WriteLine("Введите число: ");
+int N = int.Parse(Console.ReadLine());
+int count = 1;
+
+while (count <= N)
+{
+    int X = count % 2;
+    
+    if (X == 0)
+    {
+        Console.Write($"{count}, ");
+    } 
+    
+    count = count + 1;
+        
+}
